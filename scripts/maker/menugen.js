332bc08ac5e0889b3089af9fd471a46c8d572435
Hoxton <igorkalicinski@gmail.com>
2021-02-02 16:19:49 +0100

    jq

--- scripts/maker/menugen.js
+++ scripts/maker/menugen.js
@@ -1,22 +1,21 @@
 /* eslint-disable no-empty */
 /* eslint-disable require-jsdoc */
-import * as $ from '../../node_modules/jquery/dist/jquery.js'
-
-function menugen() {
-    let fs = require('fs');
-    let files = fs.readdirSync('./assets/map_tiles');
-
-    console.log(files)
+import * as $ from '../node_modules/jquery/dist/jquery.js'
 
 
+export const MenuGen = {
+    menugen: function () {
+        let fs = require('fs')
+        let files = fs.readdirSync('./assets/map_tiles')
+
+        console.log(files)
+        let i
 
 
-    for (i = 0; files.length > i; i++) {
 
-        $("tiles-picker").append("<div id='" + files[i].split(".", 1) + "'> <img src='./assets/map_tiles/" + files[i] + "'></div>")
+        for (i = 0; files.length > i; i++) {
+            $("tiles-picker").append("<div id='" + files[i].split(".", 1) + "'> <img src='./assets/map_tiles/" + files[i] + "'></div>")
+        }
     }
-
-
-
-
 }
+
