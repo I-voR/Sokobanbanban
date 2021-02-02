91244bdc55725fb3b67369accf4602c5edcbb98b
Hoxton <igorkalicinski@gmail.com>
2021-02-02 18:44:35 +0100

    button working

--- scripts/maker/tilepainter.js
+++ scripts/maker/tilepainter.js
@@ -8,21 +8,17 @@ let selectedtile
 export const TilePainter = {
     tilepaint: function (tile) {
         let tilefilename = (tile.slice(-1) == '-' ? tile.slice(0, -1) : tile) + "." + (tile.slice(-1) == '-' ? 'col' : '') + '.png'
+
         if (selectedtile == tile) {
             selectedtile = null
+            $('.grid-tile').off('click');
         } else {
-
             $('.grid-tile').click(function () {
-                console.log($(this).id)
+                console.log($(this))
                 $(this).empty()
                 $(this).append('<img src="../assets/map_tiles/' + tilefilename + '">')
             })
-
+            selectedtile = tile
         }
-
-
-
-        console.log(tile)
-        selectedtile = tile
     }
 }
