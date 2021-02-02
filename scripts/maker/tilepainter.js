d4a9d641e7bee909ec4dbe87bd6be277339b15e4
Hoxton <igorkalicinski@gmail.com>
2021-02-02 18:35:48 +0100

    maker

--- scripts/maker/tilepainter.js
+++ scripts/maker/tilepainter.js
@@ -8,12 +8,13 @@ let selectedtile
 export const TilePainter = {
     tilepaint: function (tile) {
         let tilefilename = (tile.slice(-1) == '-' ? tile.slice(0, -1) : tile) + "." + (tile.slice(-1) == '-' ? 'col' : '') + '.png'
-        if (selectedtile == tile || selectedtile == undefined || selectedtile == null) {
-
+        if (selectedtile == tile) {
+            selectedtile = null
         } else {
 
             $('.grid-tile').click(function () {
                 console.log($(this).id)
+                $(this).empty()
                 $(this).append('<img src="../assets/map_tiles/' + tilefilename + '">')
             })
 
