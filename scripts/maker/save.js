cdc7ad66717888fe502e866e8b26d5fece3e3acf
Hoxton <igorkalicinski@gmail.com>
2021-03-08 18:16:29 +0100

    graphics

--- scripts/maker/save.js
+++ scripts/maker/save.js
@@ -6,13 +6,13 @@ global.$ = global.jQuery
 import { infobox } from '../infobox.js'
 
 export const save = {
-    main: function() {
+    main: function () {
         $('.grid-tile').off('click')
         infobox.createInfobox('info', 'Select player starting position...')
 
         save.playerPlace()
 
-        $('.grid-tile').on('click', function() {
+        $('.grid-tile').on('click', function () {
             console.log(this)
             let id = this.id
 
@@ -26,7 +26,7 @@ export const save = {
             }
         })
     },
-    playerPlace: function() {
+    playerPlace: function () {
         $('.grid-tile').off('mousedown')
         $('.grid-tile').off('mouseup')
         $('.grid-tile').off('mousemove')
@@ -37,13 +37,13 @@ export const save = {
         selectionTile.css('backgroundColor', 'blue').css('opacity', '0.5').css('z-index', '100')
         selectionTile.css('position', 'absolute').css('left', 0).css('top', 0)
 
-        $('.grid-tile').on('mouseenter', function() {
+        $('.grid-tile').on('mouseenter', function () {
             $(this).append(selectionTile)
-        }).on('mouseleave', function() {
+        }).on('mouseleave', function () {
             $('selectionTile').remove()
         })
     },
-    mapRead: function(playerPos) {
+    mapRead: function (playerPos) {
         /* map save structure 30x20 tiles (26 lines):
         0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0/
         0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0/
@@ -77,18 +77,18 @@ export const save = {
         let files = fs.readdirSync('./assets/map_tiles')
         function translation(filename) {
             switch (filename) {
-            case files[0]:
-                return 0
-            case files[1]:
-                return 1
-            case files[2]:
-                return 2
-            case files[3]:
-                return 3
-            case files[4]:
-                return 4
-            default:
-                return 0
+                case files[0]:
+                    return 0
+                case files[1]:
+                    return 1
+                case files[2]:
+                    return 2
+                case files[3]:
+                    return 3
+                case files[4]:
+                    return 4
+                default:
+                    return 0
             }
         }
 
@@ -100,10 +100,8 @@ export const save = {
             let saveRow = []
             for (let x = 0; x < 30; x++) {
                 let temp = $('#' + y + '-' + x).children()[0].src.split('/')
-                //temp = temp[temp.length - 1]
                 saveRow[x] = translation(temp[temp.length - 1])
             }
-            //console.log(saveRow)
             mapData[y] = saveRow
         }
 
@@ -116,8 +114,8 @@ export const save = {
         console.log(mapData)
 
         let file = fs.createWriteStream('./created-maps/' + saveName + '.map')
-        file.on('error', function(e) { /* error handling */ })
-        mapData.forEach(function(v) { file.write(v + '\n') })
+        file.on('error', function (e) { /* error handling */ })
+        mapData.forEach(function (v) { file.write(v + '\n') })
         file.end()
     }
 }
