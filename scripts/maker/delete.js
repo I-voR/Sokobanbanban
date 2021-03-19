17c0d0e3fda8f697c495b929118681b7f614a130
iwo-strzebonski <iwo.strzebonski@gmail.com>
2021-03-19 17:17:46 +0100

    Poprawki usuwania mapy

--- scripts/maker/delete.js
+++ scripts/maker/delete.js
@@ -2,28 +2,24 @@
 
 import { funcs } from '../funcs.js'
 import { infobox } from '../infobox.js'
+
 const fs = require('fs')
+
 global.jQuery = require('jquery')
 global.$ = global.jQuery
-global.fs = require('fs')
 
 export const deleteMap = {
-    core: function () {
-
-        if ($('#load option:selected').text() == "NEW") {
+    core: () => {
+        if ($('#load option:selected').text() === 'NEW') {
             location.reload()
             return
         }
         infobox.createInfobox('remove', $('#load option:selected').text())
-        funcs.initFuncWithRemboxBool(deleteMap.deleting())
-
+        funcs.initFuncWithRemboxBool(deleteMap.deleting)
     },
-    deleting: function () {
-
-
-
+    deleting: () => {
         //wstaw pytanie czy na pewno
-        let path = "./maps/created/" + $('#load option:selected').text()
+        let path = './maps/created/' + $('#load option:selected').text()
         fs.unlink(path, (err) => {
             if (err) {
                 console.error(err)
@@ -32,7 +28,5 @@ export const deleteMap = {
         })
 
         location.reload()
-
     },
-
 }
