690df5bca8142555fd94b5081c0cc3e7b680d2ad
iwo-strzebonski <iwo.strzebonski@gmail.com>
2021-03-27 22:29:25 +0100

    Poprawki importowania jQuery DataTables

--- scripts/datatable.js
+++ scripts/datatable.js
@@ -1,43 +1,35 @@
 /* eslint-disable require-jsdoc */
 
-import { table } from 'console'
-import { funcs } from '../funcs.js'
+// import { table } from 'console'
+import { funcs } from './funcs.js'
 
 const $ = require('jquery')
+// eslint-disable-next-line no-unused-vars
+const dt = require('datatables.net')( window, $ )
 const fs = require('fs')
 
 export const data = {
     table: () => {
-
-
         let data = fs.readFileSync(funcs.cwd() + 'hall/data.csv').toString().split('\n')
 
         data.forEach((element, index) => {
             data[index] = data[index].split(',')
         })
 
-
-        $("#table").append("<thead><tr><th></th><th>" + data[0][0] + "</th><th>" + data[0][1] + "</th><th>Temp</th><th>Hum</th><th>" + data[0][2] + "</th><th>" + data[0][3] + "</th></tr></thead> <tbody>")
-        for (i = 1; i < data.length; i++) {
-
-            $("#table").append($("<tr><td> <a href='http://" + data[i][0] + "'>" + data[i][1] + "</a> </td><td>" + data[i][2] + "</td><td>" + data[i][3] + "</td></tr>"));
-
+        $('#leaderboard').append('<thead><tr><th></th><th>' + data[0][0] + '</th><th>' + data[0][1] + '</th><th>Temp</th><th>Hum</th><th>' + data[0][2] + '</th><th>' + data[0][3] + '</th></tr></thead> <tbody>')
+        for (let i = 1; i < data.length; i++) {
+            $('#leaderboard').append($('<tr><td> <a href="http://' + data[i][0] + '">' + data[i][1] + '</a> </td><td>' + data[i][2] + '</td><td>' + data[i][3] + '</td></tr>'))
         }
-        $("#table").append("</tbody>")
+        $('#leaderboard').append('</tbody>')
 
-
-
-
-        $(document).ready(function () {
+        $(function() {
             $('#leaderboard').DataTable({
-                "pageLength": 10,
-                "lengthMenu": [[10, 25, 50, -1], [10, 25, 50, "All"]]
-            });
-        });
-
+                'pageLength': 10,
+                'lengthMenu': [[10, 25, 50, -1], [10, 25, 50, 'All']]
+            })
+        })
     }
 }
-
 
 let CSVs = fs.readdirSync(funcs.cwd() + 'hall/')
 if (CSVs.length == 0) {
