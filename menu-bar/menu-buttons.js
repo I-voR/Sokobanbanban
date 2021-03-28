dec29f92be65bd93ab23e6eb129611c32791b4c4
iwo-strzebonski <iwo.strzebonski@gmail.com>
2021-03-28 19:56:26 +0200

    Kontynuacja pracy nad Dokumentacją Dodanie nowych grafik do Dokumentacji Usunięcie przycisku menu
    Signed-off-by: iwo-strzebonski <iwo.strzebonski@gmail.com>

--- menu-bar/menu-buttons.js
+++ menu-bar/menu-buttons.js
@@ -1,7 +1,6 @@
 const { remote } = require('electron')
 
 window.addEventListener('DOMContentLoaded', () => {
-    const menuButton = document.getElementById('menu-btn')
     const consoleButton = document.getElementById('console-btn')
     const minimizeButton = document.getElementById('minimize-btn')
     const closeButton = document.getElementById('close-btn')
@@ -12,10 +11,6 @@ window.addEventListener('DOMContentLoaded', () => {
             window.home()
         })
     }
-
-    menuButton.addEventListener('click', () => {
-        window.openMenu(0, 30)
-    })
 
     consoleButton.addEventListener('click', () => {
         remote.getCurrentWindow().webContents.openDevTools()
